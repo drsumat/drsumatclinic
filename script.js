d06084fa00227d4d8532d1603a9e958ac8fa11b6
drsumat <ramsumatmrj@gmail.com>
2026-07-27 23:01:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Website loaded');
+// Fade-in animation
+
+document.addEventListener("DOMContentLoaded", () => {
+
+const elements = document.querySelectorAll(
+".doctor-card, .feature-card, .action-btn, .disease-list span"
+);
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+
+if (entry.isIntersecting) {
+entry.target.classList.add("show");
+}
+
+});
+
+}, {
+threshold: 0.15
+});
+
+elements.forEach((el) => {
+el.classList.add("hidden");
+observer.observe(el);
+});
+
+});
